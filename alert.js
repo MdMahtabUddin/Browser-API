f4c03bf5cc0d6cc22a3a94a8405a3ef4961633f1
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-09-06 19:56:33 +0600

    practice

--- alert.js
+++ alert.js
@@ -2,24 +2,36 @@ console.log('i am here');
 
 alert('Ma is coming!!');
 
-// const maComing = () => {
-//     alert('Ammu is coming. Open the book');
-// }
+const maComing = () => {
+    alert('Ammu is coming. Open the book');
+}
 
-// const askPicnic = () => {
-//     const response = confirm('Are you going to picnic?');
-//     console.log(response);
-//     if (response === true) {
-//         alert('amake fee ta bKash kore de');
-//     }
-//     else {
-//         console.log('DGM !!! Door e giya mor!!!');
-//     }
-// }
+const askPicnic = () => {
+    const response = confirm('Are you going to picnic?');
+    console.log(response);
+    if (response === true) {
+        alert('amake fee ta bKash kore de');
+    }
+    else {
+        console.log('DGM !!! Door e giya mor!!!');
+    }
+}
 
-// const askName = () => {
-//     const name = prompt('What is your name?');
-//     if (name) {
-//         console.log(name)
-//     }
-// }
+const askName = () => {
+    const name = prompt('What is your name?');
+    if (name) {
+        console.log(name)
+    }
+}
+
+const marryMe =()=> {
+    const response = confirm('you are agree');
+    console.log(response);
+    if (response === true){
+        alert('cholo biye kore feli');
+
+    } else{
+        console.log('Dure gia mor');
+    }
+
+    }
